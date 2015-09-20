78028a6d12f8e14739dcc31e43efbca76115c5dd
Will Han <Will@Wills-Mac-mini.local>
2015-09-20 19:23:03 +0800

    Hide the app icon in dock and show menu status bar.

--- DailyNote/AppDelegate.m
+++ DailyNote/AppDelegate.m
@@ -8,18 +8,77 @@
 
 #import "AppDelegate.h"
 
+typedef NS_OPTIONS(NSUInteger, DNMenuItemKind) {
+    DNMenuItemKindMain,
+    DNMenuItemKindPreferences,
+    DNMenuItemKindQuit
+};
+
 @interface AppDelegate ()
+{
+    NSStatusItem *statusItem;
+}
 
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    
+    [self initStatusBarConfig];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
+
+#pragma mark - 
+
+- (void)initStatusBarConfig
+{
+    NSStatusBar *bar = [NSStatusBar systemStatusBar];
+    statusItem = [bar statusItemWithLength:NSVariableStatusItemLength];
+    
+    statusItem.title = @"Note";
+    statusItem.highlightMode = YES;
+    
+    NSMenu *menu = [[NSMenu alloc] init];
+    statusItem.menu = menu;
+    
+    NSMenuItem *item = nil;
+    
+    item = [[NSMenuItem alloc] initWithTitle:@"Main Window" action:@selector(menuItemClicked:) keyEquivalent:@""];
+    item.tag = DNMenuItemKindMain;
+    [menu addItem:item];
+    item = [[NSMenuItem alloc] initWithTitle:@"Preferences" action:@selector(menuItemClicked:) keyEquivalent:@""];
+    item.tag = DNMenuItemKindPreferences;
+    [menu addItem:item];
+    item = [[NSMenuItem alloc] initWithTitle:@"Quit" action:@selector(menuItemClicked:) keyEquivalent:@""];
+    item.tag = DNMenuItemKindQuit;
+    [menu addItem:item];
+}
+
+- (void)menuItemClicked:(id)sender
+{
+    if (!sender) {
+        return;
+    }
+    
+    NSAssert([sender isKindOfClass:[NSMenuItem class]], @"Unexpected sender, sender: %@, func: %s", sender, __func__);
+    NSMenuItem *item = (NSMenuItem *)sender;
+    
+    switch (item.tag) {
+        case DNMenuItemKindMain:
+            
+            break;
+        case DNMenuItemKindPreferences:
+            
+            break;
+        case DNMenuItemKindQuit:
+            exit(EXIT_SUCCESS);
+            break;
+    }
+}
+
 
 @end
